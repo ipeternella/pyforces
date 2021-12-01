04b53dcae462f56646c9d72a678a25fa450aea28
IgooorGP <igor.feq@gmail.com>
2021-11-30 22:20:00 -0300

    chore(heaps): add heap powers

---
+++ powers/heap.py
@@ -0,0 +1,143 @@
+"""
+Simple and short min and max heap implementations to use as Python's heapq module
+only contains a min heap implementation.
+"""
+import math
+from typing import Generic
+from typing import List
+from typing import Optional
+from typing import TypeVar
+
+T = TypeVar("T")
+
+
+class MaxHeap(Generic[T]):
+    heap: List[T]
+
+    def __init__(self):
+        self.heap = []
+
+    def __len__(self) -> int:
+        return len(self.heap)
+
+    def parent(self, i: int) -> int:
+        return math.ceil(i / 2) - 1
+
+    def left_child(self, i: int) -> int:
+        return 2 * i + 1
+
+    def right_child(self, i: int) -> int:
+        return 2 * i + 2
+
+    def increase_key(self, i: int, new_key: T) -> None:
+        if new_key < self.heap[i]:  # type: ignore
+            raise ValueError(f"Key {new_key} is less than {self.heap[i]}")
+
+        self.heap[i] = new_key
+        while i > 0 and self.heap[i] > self.heap[self.parent(i)]:  # type: ignore
+            self._swap(i, self.parent(i))
+            i = self.parent(i)  # dont store parent_it or when i changes, parent_it must be updated!
+
+    def insert(self, key: T):
+        self.heap.append(key)
+        self.increase_key(len(self) - 1, key)
+
+    def heapify(self, parent_i: int) -> None:
+        left_i = self.left_child(parent_i)
+        right_i = self.right_child(parent_i)
+        biggest_i = parent_i
+        size = len(self.heap)
+
+        if left_i < size and self.heap[parent_i] < self.heap[left_i]:  # type: ignore
+            biggest_i = left_i
+
+        if right_i < size and self.heap[biggest_i] < self.heap[right_i]:  # type: ignore
+            biggest_i = right_i
+
+        if biggest_i != parent_i:
+            self._swap(biggest_i, parent_i)
+            self.heapify(parent_i)
+
+    def pop_max(self) -> Optional[T]:
+        size = len(self)
+
+        if size < 1:
+            return None
+
+        key = self.heap[0]
+        self._swap(0, size - 1)
+        self.heap.pop()
+        self.heapify(0)
+
+        return key
+
+    def _swap(self, i: int, j: int) -> None:
+        tmp = self.heap[i]
+        self.heap[i] = self.heap[j]
+        self.heap[j] = tmp
+
+
+class MinHeap(Generic[T]):
+    heap: List[T]
+
+    def __init__(self):
+        self.heap = []
+
+    def __len__(self) -> int:
+        return len(self.heap)
+
+    def parent(self, i: int) -> int:
+        return math.ceil(i / 2) - 1
+
+    def left_child(self, i: int) -> int:
+        return 2 * i + 1
+
+    def right_child(self, i: int) -> int:
+        return 2 * i + 2
+
+    def decrease_key(self, i: int, new_key: T) -> None:
+        if new_key > self.heap[i]:  # type: ignore
+            raise ValueError(f"Key {new_key} is bigger than {self.heap[i]}")
+
+        self.heap[i] = new_key
+        while i > 0 and self.heap[i] < self.heap[self.parent(i)]:  # type: ignore
+            self._swap(i, self.parent(i))
+            i = self.parent(i)  # dont store parent_it or when i changes, parent_it must be updated!
+
+    def insert(self, key: T):
+        self.heap.append(key)
+        self.decrease_key(len(self) - 1, key)
+
+    def heapify(self, parent_i: int) -> None:
+        left_i = self.left_child(parent_i)
+        right_i = self.right_child(parent_i)
+        smallest_i = parent_i
+        size = len(self.heap)
+
+        if left_i < size and self.heap[parent_i] > self.heap[left_i]:  # type: ignore
+            smallest_i = left_i
+
+        if right_i < size and self.heap[smallest_i] > self.heap[right_i]:  # type: ignore
+            smallest_i = right_i
+
+        if smallest_i != parent_i:
+            self._swap(smallest_i, parent_i)
+            self.heapify(parent_i)
+
+    def pop_min(self) -> Optional[T]:
+        size = len(self)
+
+        if size < 1:
+            return None
+
+        key = self.heap[0]
+        self._swap(0, size - 1)
+        self.heap.pop()
+        self.heapify(0)
+
+        return key
+
+    def _swap(self, i: int, j: int) -> None:
+        tmp = self.heap[i]
+        self.heap[i] = self.heap[j]
+        self.heap[j] = tmp
---
+++ tests/powers/test_heap.py
@@ -0,0 +1,118 @@
+import unittest
+
+from powers.heap import MaxHeap
+from powers.heap import MinHeap
+
+
+class HeapTests(unittest.TestCase):
+    def test_should_build_max_heap(self):
+        # arrange
+        heap = MaxHeap()
+
+        # act
+        heap.insert(1)
+        heap.insert(2)
+        heap.insert(3)
+        heap.insert(4)
+
+        # assert
+        self.assertEqual(heap.heap, [4, 3, 2, 1])
+
+    def test_should_build_min_heap(self):
+        # arrange
+        heap = MinHeap()
+
+        # act
+        heap.insert(3)
+        heap.insert(1)
+        heap.insert(4)
+        heap.insert(2)
+
+        # assert
+        self.assertEqual(heap.heap, [1, 2, 4, 3])
+
+    def test_should_pop_items_from_min_heap(self):
+        # arrange
+        heap = self.build_min_heap_one()
+
+        # act and assert
+        self.assertEqual(heap.pop_min(), 1)
+        self.assertEqual(heap.heap, [2, 3, 4])
+
+        self.assertEqual(heap.pop_min(), 2)
+        self.assertEqual(heap.heap, [3, 4])
+
+        self.assertEqual(heap.pop_min(), 3)
+        self.assertEqual(heap.heap, [4])
+
+        self.assertEqual(heap.pop_min(), 4)
+        self.assertEqual(heap.heap, [])
+
+        self.assertIsNone(heap.pop_min())
+
+    def test_should_pop_items_from_max_heap(self):
+        # arrange
+        heap = self.build_max_heap_one()
+
+        # act and assert
+        self.assertEqual(heap.pop_max(), 4)
+        self.assertEqual(heap.heap, [3, 1, 2])
+
+        self.assertEqual(heap.pop_max(), 3)
+        self.assertEqual(heap.heap, [2, 1])
+
+        self.assertEqual(heap.pop_max(), 2)
+        self.assertEqual(heap.heap, [1])
+
+        self.assertEqual(heap.pop_max(), 1)
+        self.assertEqual(heap.heap, [])
+
+        self.assertIsNone(heap.pop_max())
+
+        # act - reinsert items
+        heap.insert(5)
+        heap.insert(1)
+        heap.insert(12)
+        heap.insert(30)
+
+        # assert
+        self.assertEqual(heap.heap, [30, 12, 5, 1])
+
+        # act
+        self.assertEqual(heap.pop_max(), 30)
+        self.assertEqual(heap.heap, [12, 1, 5])
+
+        self.assertEqual(heap.pop_max(), 12)
+        self.assertEqual(heap.heap, [5, 1])
+
+        # act
+        heap.insert(17)
+
+        # assert
+        self.assertEqual(heap.heap, [17, 1, 5])
+
+        heap.increase_key(2, 20)
+        self.assertEqual(heap.heap, [20, 1, 17])
+
+        heap.increase_key(1, 30)
+        self.assertEqual(heap.heap, [30, 20, 17])
+
+    def build_max_heap_one(self) -> MaxHeap[int]:
+        heap = MaxHeap()
+
+        heap.insert(1)
+        heap.insert(2)
+        heap.insert(3)
+        heap.insert(4)
+
+        return heap
+
+    def build_min_heap_one(self):
+        heap = MinHeap()
+
+        heap.insert(3)
+        heap.insert(1)
+        heap.insert(4)
+        heap.insert(2)
+
+        return heap
