a3a2bd84f4c6c47bb1f576e10faafd37ea635dc3
IgooorGP <igor.feq@gmail.com>
2021-12-02 15:22:01 -0300

    chore(powers): add heapsort variations

--- powers/sorting/heap.py
+++ powers/sorting/heap.py
@@ -1,9 +1,11 @@
 """
 Module with heapsort which runs in O(NlogN).
 """
+import heapq
 from typing import List
 
 from powers.heap import MaxHeap
+from powers.heap import MinHeap
 
 
 def swap(arr: List, i: int, j: int) -> None:
@@ -21,3 +23,19 @@ def heapsort(arr: List) -> List:
         max_heap.heapify(0, i)  # keep the heap! i reduces the size of the heap!
 
     return arr
+
+
+def heapsort_with_minheap(arr: List) -> List:
+    n = len(arr)
+    min_heap = MinHeap(arr)
+
+    # pops min values which automatically keeps heap invariant and returns values in asc order
+    return [min_heap.pop_min() for _ in range(n)]
+
+
+def heapsort_builtin(arr: List[int]) -> List:
+    n = len(arr)
+    heapq.heapify(arr)  # heapifies the list
+
+    # pops min values
+    return [heapq.heappop(arr) for _ in range(n)]
--- tests/powers/sorting/test_heap.py
+++ tests/powers/sorting/test_heap.py
@@ -3,6 +3,8 @@ import unittest
 from parameterized import parameterized
 
 from powers.sorting.heap import heapsort
+from powers.sorting.heap import heapsort_builtin
+from powers.sorting.heap import heapsort_with_minheap
 
 
 class HeapSortTests(unittest.TestCase):
@@ -14,7 +16,33 @@ class HeapSortTests(unittest.TestCase):
     )
     def test_should_sort_numbers(self, nums, expected_sorted_nums):
         # act
-        heapsort(nums)
+        heapsort(nums)  # in-place
+
+        # assert
+        self.assertEqual(nums, expected_sorted_nums)
+
+    @parameterized.expand(
+        [
+            ([4, 3, -1, 0, 2], [-1, 0, 2, 3, 4]),
+            ([3, 1, 1, 2, 2], [1, 1, 2, 2, 3]),
+        ]
+    )
+    def test_should_sort_numbers_with_minheap(self, nums, expected_sorted_nums):
+        # act
+        nums = heapsort_with_minheap(nums)  # returns new list
+
+        # assert
+        self.assertEqual(nums, expected_sorted_nums)
+
+    @parameterized.expand(
+        [
+            ([4, 3, -1, 0, 2], [-1, 0, 2, 3, 4]),
+            ([3, 1, 1, 2, 2], [1, 1, 2, 2, 3]),
+        ]
+    )
+    def test_should_sort_numbers_with_builtin_minheap(self, nums, expected_sorted_nums):
+        # act
+        nums = heapsort_builtin(nums)
 
         # assert
         self.assertEqual(nums, expected_sorted_nums)
