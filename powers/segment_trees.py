55d8ce823f990d2ae784fcfc5c22a840a8796a5f
IgooorGP <igor.feq@gmail.com>
2021-12-12 17:34:41 -0300

    chore(powers): add segment trees (wip)

--- powers/segment_trees.py
+++ powers/segment_trees.py
@@ -1,14 +1,9 @@
 """
-Module with segment trees also known as statistic trees. A segment tree contains nodes
-in which node holds a statistic info regarding some intervals (segments). It can be built
-in O(NlogN) and allows querying info from intervals in O(logN + K) where K is the number of intervals.
+Module with segment (statistic trees). A segment tree contains nodes in which each node 
+holds a statistic info that aggregates info of some intervals (segments).
 
-
-# (l, r) -> from [left, right] is the query range
-
-# partial overlap -> go to the right and the left and return sum
-# total overlap -> return value
-# no overlap -> return "0"
+Tree can be built in  in O(NlogN) and allows querying info from intervals in O(logN + K)
+where K is the number of intervals.
 """
 from typing import Callable
 from typing import Generic
@@ -29,20 +24,52 @@ class SegmentTree(Generic[T]):
         self.values = values
         self._build(0, len(values) - 1, 0)
 
-    def _build(self, start: int, end: int, node_i: int) -> None:
+    def _build(self, start: int, end: int, tree_i: int) -> None:
         # divide and conquer: leaf nodes just contain the bare values
         if start >= end:
-            self.stree[node_i] = self.values[start]
+            self.stree[tree_i] = self.values[start]
             return
 
         mid = (start + end) // 2  # defines segments of the tree
-        self._build(start, mid, 2 * node_i + 1)  # left child
-        self._build(mid + 1, end, 2 * node_i + 2)  # right child
+        self._build(start, mid, 2 * tree_i + 1)  # left child
+        self._build(mid + 1, end, 2 * tree_i + 2)  # right child
 
-        self.stree[node_i] = self.statistic_fn((self.stree[2 * node_i + 1], self.stree[2 * node_i + 2]))
+        # parent
+        self.stree[tree_i] = self.statistic_fn((self.stree[2 * tree_i + 1], self.stree[2 * tree_i + 2]))
 
     def query(self, left: int, right: int):
-        def _query(start: int, end: int, left: int, right: int, node_i: int):
-            pass
+        def _query(node_start: int, node_end: int, tree_i: int):
+            if node_start > right or node_end < left:  # no segment overlapping
+                return 0
 
-        return _query(0, len(self.values) - 1, left, right, 0)
+            if node_start >= left and node_end <= right:  # full segment overlap
+                return self.stree[tree_i]
+
+            # partial segment overlap
+            mid = (node_start + node_end) // 2
+            query_1 = _query(node_start, mid, 2 * tree_i + 1)
+            query_2 = _query(mid + 1, node_end, 2 * tree_i + 2)
+
+            return self.statistic_fn((query_1, query_2))
+
+        return _query(0, len(self.values) - 1, 0)
+
+    def update(self, i: int, new_value: T) -> None:
+        def _update(node_start: int, node_end: int, tree_i: int):
+            # base case
+            if node_start == node_end:
+                self.stree[tree_i] = new_value
+                return
+
+            mid = (node_start + node_end) // 2
+            if i <= mid:
+                _update(node_start, mid, 2 * tree_i + 1)
+            else:
+                _update(mid + 1, node_end, 2 * tree_i + 2)
+
+            # aggregate parent node using statistic fn
+            left_child = self.stree[2 * tree_i + 1]
+            right_child = self.stree[2 * tree_i + 2]
+            self.stree[tree_i] = self.statistic_fn((left_child, right_child))
+
+        _update(0, len(self.values) - 1, 0)
