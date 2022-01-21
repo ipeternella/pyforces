ea6e72723e2aace99049658fb7d1b85da77f4f1c
IgooorGP <igor.feq@gmail.com>
2022-01-21 16:30:56 -0300

    chore(lc): improve LC#875

--- challenges/leetcode/koko_eating_bananas.py
+++ challenges/leetcode/koko_eating_bananas.py
@@ -1,3 +1,8 @@
+"""
+Solution for LC#875: Koko Eating Bananas
+
+https://leetcode.com/problems/koko-eating-bananas/
+"""
 import math
 from typing import List
 
@@ -9,7 +14,29 @@ class Solution:
             for bananas in piles:
                 t += math.ceil(bananas / k)
 
-            return max(t, h)
+            return t
+
+        min_k, max_k = 1, max(piles)
+        while min_k < max_k:
+            mid_k = (min_k + max_k) // 2
+            t = koko_eating_time(piles, mid_k)
+
+            if t <= h:  # eating too fast, so t <= h: decrease k
+                max_k = mid_k
+            else:
+                min_k = mid_k + 1  # eating too slow, so t > h: increase k
+
+        return min_k
+
+
+class SolutionII:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def koko_eating_time(piles, k):
+            t = 0
+            for bananas in piles:
+                t += math.ceil(bananas / k)
+
+            return max(t, h)  # if t < h, than wait until h
 
         min_k, max_k = 1, max(piles)
         k = max_k
