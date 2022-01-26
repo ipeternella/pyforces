23da46d7a0100369b767c413a8001121b2339259
IgooorGP <igor.feq@gmail.com>
2022-01-26 11:14:44 -0300

    chore(lc): improve LC#45

--- challenges/leetcode/jump_game_ii.py
+++ challenges/leetcode/jump_game_ii.py
@@ -9,26 +9,17 @@ from typing import List
 INF = sys.maxsize
 
 
-class SolutionDP:
-    """
-    [!]: This is not an optimal solution yet: there's a greedy approach which naturally
-         is much faster than a tabulated DP approach.
-    """
-
+class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)
-        if n < 2:
-            return 0
 
+        # state: dp[i index] = min jumps
         dp = [INF] * n
-        dp[n - 1] = 0
-        dp[n - 2] = 1
+        dp[0] = 0
 
-        for i in range(n - 3, -1, -1):
-            max_jump_size = nums[i]
-            for jump_size in range(max_jump_size, 0, -1):
+        for j in range(n):
+            jumps = min(nums[j], n - j - 1)  # clamping, max possible jump size: n - 1 - j
+            for jump in range(1, jumps + 1):
+                dp[j + jump] = min(dp[j + jump], dp[j] + 1)
 
-                if i + jump_size <= n - 1:
-                    dp[i] = min(dp[i], dp[i + jump_size] + 1)
-
-        return dp[0]
+        return dp[n - 1]
