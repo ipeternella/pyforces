6dc694bbb7ea35879441b6f1ca7b29973911f338
IgooorGP <igor.feq@gmail.com>
2022-02-02 13:06:24 -0300

    chore(lc): add LC#746

---
+++ challenges/leetcode/min_cost_climbing_stairs.py
@@ -0,0 +1,17 @@
+"""
+Solution for LC#746: Min Cost Climbing Stairs
+
+https://leetcode.com/problems/min-cost-climbing-stairs/
+"""
+from typing import List
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = [0] * (n + 1)
+
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1])
+
+        return dp[n]
