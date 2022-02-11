3df94b90539c14fe6eaba6369109a481ddbde942
IgooorGP <igor.feq@gmail.com>
2022-02-11 11:42:52 -0300

    chore(lc): improve LC#567

--- challenges/leetcode/permutation_in_string.py
+++ challenges/leetcode/permutation_in_string.py
@@ -3,53 +3,36 @@ Solution for LC#567: Permutation in String
 
 https://leetcode.com/problems/permutation-in-string/
 """
-from typing import Dict
-
-
-def add_freq(key: str, d: Dict, required: Dict):
-    if key in required:
-        if key in d:
-            d[key] += 1
-        else:
-            d[key] = 1
-
-
-def remove_freq(key: str, d: Dict, required: Dict):
-    if key in required:
-        d[key] -= 1
+from collections import Counter
 
 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         n = len(s1)
         m = len(s2)
-        required: Dict[str, int] = dict()
-        freqs: Dict[str, int] = dict()
 
         if n > m:
-            return False  # pattern > str: impossible
+            return False
 
-        # parses s1 to get freq. distribution of the pattern
-        for ch in s1:
-            required[ch] = 1 if ch not in required else required[ch] + 1
+        s1_chars = Counter(s1)
+        s2_window = Counter(s2[0:n])  # first window
 
-        # first window - [0, n)
-        for i in range(0, n):
-            ch = s2[i]
-            add_freq(ch, freqs, required)
-
-        if freqs == required:
+        if s1_chars == s2_window:
             return True
 
-        for i in range(n, m):
-            # sliding windows - [n, m)
-            ch_out = s2[i - n]
-            ch_in = s2[i]
+        for i in range(1, m - n + 1):
+            # sliding window update
+            old_ch = s2[i - 1]
+            new_ch = s2[i + n - 1]
 
-            remove_freq(ch_out, freqs, required)  # out of the window: remove freq
-            add_freq(ch_in, freqs, required)  # in the window: add freq
+            s2_window[new_ch] += 1
+            s2_window[old_ch] -= 1
 
-            if required == freqs:
+            if s2_window[old_ch] <= 0:
+                s2_window.pop(old_ch)
+
+            # check if chars frequency match == anagram!
+            if s1_chars == s2_window:
                 return True
 
         return False
