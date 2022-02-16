db34b374cd52e42a45c798ec07b97f34622c5e7f
IgooorGP <igor.feq@gmail.com>
2022-02-16 12:28:43 -0300

    chore(lc): add dp tabulation for LC#322

--- challenges/leetcode/coin_change.py
+++ challenges/leetcode/coin_change.py
@@ -8,8 +8,28 @@ from typing import List
 
 INF = sys.maxsize
 
-# dp approach with memoization
+# dp approach with tabulation (bottom-up)
 class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [INF] * (amount + 1)
+        dp[0] = 0
+
+        for coin in coins:
+            if coin <= amount:
+                dp[coin] = 1
+
+        # tabulation
+        for k in range(amount + 1):
+            for coin in coins:
+                if k - coin >= 0:
+                    dp[k] = min(dp[k], dp[k - coin] + 1)
+
+        # if dp state == INF it means such amount was never reached so we return -1
+        return dp[amount] if dp[amount] != INF else -1
+
+
+# dp approach with memoization (top-down)
+class SolutionMemoized:
     def coinChange(self, coins: List[int], amount: int) -> int:
         def change(target, dp):
             if target == 0:
@@ -32,4 +52,6 @@ class Solution:
 
         dp = [-1] * (amount + 1)
         c = change(amount, dp)
+
+        # if dp state == INF it means such amount was never reached so we return -1
         return c if c != INF else -1
