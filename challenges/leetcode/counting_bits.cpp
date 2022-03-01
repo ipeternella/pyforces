0f7909d1f280fdc91de18917573375307c308106
IgooorGP <igor.feq@gmail.com>
2022-03-01 11:21:16 -0300

    chore(lc): add LC#338 in cpp

---
+++ challenges/leetcode/counting_bits.cpp
@@ -0,0 +1,28 @@
+/*
+ * Solution for LC#338: Counting Bits
+ *
+ * https://leetcode.com/problems/counting-bits/
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n + 1, 0);
+        long long int pow2 = 1;
+        long long int last_pow2 = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (i == pow2) {
+                dp[i] = 1;
+                last_pow2 = pow2;
+                pow2 *= 2;
+            } else {
+                dp[i] = 1 + dp[i - last_pow2];
+            }
+        }
+
+        return dp;
+    }
+};
