ee2d29d304ff92e0d05aa47b00e58908a49545bf
IgooorGP <igor.feq@gmail.com>
2022-03-09 22:47:08 -0300

    chore(templates): add cpp template

---
+++ templates/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+#ifdef LOCAL_ONLY
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+
+    return 0;
+}
