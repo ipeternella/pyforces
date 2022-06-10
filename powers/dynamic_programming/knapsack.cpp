88414e929dbc5de26995dfe3c51f9002a0be716d
ipeternella <igor.feq@gmail.com>
2022-06-09 23:07:17 -0300

    chore(powers): add knapsack variations

---
+++ powers/dynamic_programming/knapsack.cpp
@@ -0,0 +1,149 @@
+/*
+ * Knapsack-related problems.
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+#define DEBUG false
+
+/*
+ * Knapsack 01: items can be picked only once and NO repetitions are allowed. DP state
+ * requires 2 variables: dp[i][w] <- value
+ *
+ * Time complexity: O(N * W)
+ * Space complexity: O(N * W)  -- can be improved, if necessary
+ */
+int knapsack_01(vector<int> weights, vector<int> values, int W) {
+    int n = values.size();
+    vector<vector<int>> dp(n, vector<int>(W + 1));
+
+    // init dp state
+    for (int i = 0; i < n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (weights[i] <= w) {
+                dp[i][w] = values[i];
+            } else {
+                // pick biggest value of knapsack with one less value
+                dp[i][w] = i - 1 >= 0 ? dp[i - 1][w] : 0;
+            }
+        }
+    }
+
+    for (int i = 1; i < n; i++) {
+        for (int w = 0; w <= W; w++) {
+            if (w - weights[i] >= 0)
+                dp[i][w] = max(dp[i - 1][w - weights[i]] + values[i], dp[i - 1][w]);
+        }
+    }
+
+    if (DEBUG) {
+        for (int i = 0; i < n; i++) {
+            for (int w = 0; w <= W; w++) {
+                cout << dp[i][w] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    return dp[n - 1][W];
+}
+
+/*
+ * Knapsack Fractional: items can only be picked once but they can be broken to fit into
+ * the knapsack. No DP is needed as a greedy algorithm applies here.
+ *
+ * Time complexity: O(NlogN) -- sorting by ratio
+ * Space complexity: O(N)
+ */
+struct Item {
+    int value;
+    int weight;
+    double ratio;
+};
+
+double knapsack_fractional(vector<int> weights, vector<int> values, int W) {
+    auto sort_by_ratio = [](Item item1, Item item2) { return item1.ratio > item2.ratio; };
+    int n = values.size();
+    vector<Item> items;  // easier to sort with a struct with a vi, wi and ratio
+    double value = 0.0;
+
+    for (int i = 0; i < n; i++) {
+        Item item = {values[i], weights[i], (double)values[i] / weights[i]};
+        items.push_back(item);
+    }
+
+    // sort by highest v/w ratio -- greedy strategy applies so no DP is needed
+    sort(items.begin(), items.end(), sort_by_ratio);
+
+    for (int i = 0; i < n; i++) {
+        if (items[i].weight <= W) {
+            // item fits the bag, put it as a whole
+            W -= items[i].weight;
+            value += items[i].value;
+        } else {
+            // item is heavier than remaining W -- add its fractional (wi > W)
+            value += ((double)W / items[i].weight) * items[i].value;
+        }
+    }
+
+    return value;
+}
+
+/*
+ * Knapsack Unbound: each item can be picked indefinitely (as many times as we can fit it into
+ * the knapsack). As such, there's no need for a DP with two states due to the fact that we must
+ * must always look all the items. DP with a single state is enough: dp[w] <- value
+ *
+ * Time complexity: O(N * W)
+ * Space complexity: O(W)
+ */
+int knapsack_unbound(vector<int> weights, vector<int> values, int W) {
+    int n = values.size();
+    vector<int> dp(W + 1, 0);
+
+    for (int w = 0; w <= W; w++) {
+        for (int i = 0; i < n; i++) {
+            // need to check all the items all the time (they are always available)
+            if (weights[i] <= w)
+                dp[w] = max(dp[w], dp[w - weights[i]] + values[i]);  // pick or not
+        }
+    }
+
+    return dp[W];
+}
+
+int main() {
+#ifdef LOCAL_ONLY
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    int W, n;
+    cin >> W >> n;
+
+    vector<int> weights;
+    vector<int> values;
+
+    while (n--) {
+        int w;
+        cin >> w;
+        weights.push_back(w);
+    }
+
+    n = weights.size();
+    while (n--) {
+        int v;
+        cin >> v;
+        values.push_back(v);
+    }
+
+    cout << "Knapsack 01 (DP 2 variables)" << endl;
+    cout << knapsack_01(weights, values, W) << endl;
+
+    cout << "Knapsack Fractional (Greedy)" << endl;
+    cout << knapsack_fractional(weights, values, W) << endl;
+
+    cout << "Knapsack Unbound (DP 1 variable)" << endl;
+    cout << knapsack_unbound(weights, values, W);
+
+    return 0;
+}
